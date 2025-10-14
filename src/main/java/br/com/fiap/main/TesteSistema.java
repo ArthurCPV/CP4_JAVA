b85c6754e68bf3f482f223a098517b30122cee74
Vitor Dalmagro <vitordalmagro77@gmail.com>
2025-10-14 15:45:14 -0300

    a

--- src/main/java/br/com/fiap/main/TesteSistema.java
+++ src/main/java/br/com/fiap/main/TesteSistema.java
@@ -17,20 +17,13 @@ public class TesteSistema {
         do {
 
             System.out.println("*** Menu Princpal *** \n1 - API => Jogos de Playstation 4\n2 = API => Jogos de XBOX \n3 - API => Jogos de Nintendo Switch\n4 - API - Coloca tua api aqui arthur\n5 - Sair do sistema");
-            System.out.println("Escolha: ");
+            System.out.print("Escolha: ");
             int escolhaMenuPrincipal = scanner.nextInt();
 
-            if (escolhaMenuPrincipal == 1){
-                MetodoPlaystation.escolherJogoPlaystation();
-            } else if (escolhaMenuPrincipal == 2) {
-                MetodoXbox.escolherJogoXbox();
-            } else if (escolhaMenuPrincipal == 3) {
-
-            }else if (escolhaMenuPrincipal == 4){
-
-            } else if (escolhaMenuPrincipal == 5) {
-                saidaSistema = true;
-            }
+           switch (escolhaMenuPrincipal){
+               case 1 : MetodoPlaystation.escolherJogoPlaystation();
+               case 5 : saidaSistema = true;
+           }
 
         }while (!saidaSistema);
 
